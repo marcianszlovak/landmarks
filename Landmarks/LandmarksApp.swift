facf5d23370fc414c167a516807e36341280bcc6
Marcian Szlovak <marcianszlovak@gmail.com>
2023-04-14 23:16:11 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Marcian Szlovak on 2023. 04. 14..
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
