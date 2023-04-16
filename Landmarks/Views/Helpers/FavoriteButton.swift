eadf194aab358cf559ba3069ee81173bf11b90a6
Marcian Szlovak <marcianszlovak@gmail.com>
2023-04-16 21:20:05 +0200

    (tutorial-pt3) handling user input

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Marcian Szlovak on 2023. 04. 16..
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .purple : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
