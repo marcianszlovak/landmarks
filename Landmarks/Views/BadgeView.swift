30c3e8eb3a37a769d8f671561ebfbff848f80bc5
Marcian Szlovak <marcianszlovak@gmail.com>
2023-04-17 18:50:35 +0200

    (tutorial-pt4) drawing paths and shapes

---
+++ Landmarks/Views/BadgeView.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeView.swift
+//  Landmarks
+//
+//  Created by Marcian Szlovak on 2023. 04. 17..
+//
+
+import SwiftUI
+
+struct BadgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeView_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeView()
+    }
+}
